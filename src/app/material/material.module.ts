661815755e1cd5644e67def661fc25f764182d23
Josebratt <josebratt@gmail.com>
2022-06-15 17:21:42 -0400

    Update material.module.ts

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 const Material = [
-  MatButtonModule
+  MatButtonModule,
+  MatToolbarModule
 ];
 
 @NgModule({
