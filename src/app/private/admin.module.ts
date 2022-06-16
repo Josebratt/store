689d001f1e358e6f5332fe38cfbd40c5e9b13cfd
Josebratt <josebratt@gmail.com>
2022-06-16 12:07:57 -0400

    Update admin.module.ts

--- src/app/private/admin.module.ts
+++ src/app/private/admin.module.ts
@@ -7,13 +7,15 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin/admin.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProductsComponent } from './products/products.component';
 
 
 @NgModule({
   declarations: [
     AdminComponent,
     SidebarComponent,
-    DashboardComponent
+    DashboardComponent,
+    ProductsComponent
   ],
   imports: [
     CommonModule,
